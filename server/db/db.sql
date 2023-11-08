a4f86f6ce142b883ee33fc0322c5d817d92dd938
adelaide20 <adelaiderorisang@gmail.com>
2023-11-08 21:58:06 +0200

    generated the database script

---
+++ server/db/db.sql
@@ -0,0 +1,36 @@
+-- admin login details table
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE  users  (
+   id SERIAL PRIMARY KEY UNIQUE,
+   email varchar(255) UNIQUE,
+   password varchar (255),
+   name varchar(255)
+);
+
+
+-- employee details table
+DROP TABLE IF EXISTS employees CASCADE;
+CREATE TABLE  employees  (
+   emp_id SERIAL PRIMARY KEY UNIQUE,
+   first_name varchar (255),
+   last_name varchar (255),
+   email varchar(255) UNIQUE,
+   gender varchar(255),
+   contactno varchar (15)
+);
+
+
+-- employment details table
+DROP TABLE IF EXISTS employment CASCADE;
+CREATE TABLE  employment  (
+   id SERIAL PRIMARY KEY UNIQUE,
+   employee varchar(255),
+   position varchar(255),
+   emp_status varchar(255) DEFAULT active,
+   start_date date,
+   salary varchar(255),
+   end_date date,
+   visibility BOOLEAN DEFAULT false,
+   FOREIGN KEY (employee) REFERENCES employees(emp_id)
+);
+
