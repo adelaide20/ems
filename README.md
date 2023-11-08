8f02742bf4683c73f851a5965678ce051c864219
adelaide20 <adelaiderorisang@gmail.com>
2023-11-08 21:41:17 +0200

    updated the reame file

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # ems
 
+
+### tech stach
+frontend: angular && bootstrap
+backend: nodejs && express
+database: postgresql
+devops: render && vercel
+
+
 ### running the server
 npm run app
--- server/config/db.config.js
+++ server/config/db.config.js
@@ -8,11 +8,16 @@ const pool = new Pool({
     port: 5432,
 })
 
-// connect to this database
-// postgres://admin:SgAFmlnnVtIp6AeglxUevzertTWBOtTw@dpg-cl5tuvt6fh7c73cto5e0-a.oregon-postgres.render.com/ems_6oyk
+// const pool = new Pool({
+//     user: 'admin',
+//     host: 'SgAFmlnnVtIp6AeglxUevzertTWBOtTw@dpg-cl5tuvt6fh7c73cto5e0-a.oregon-postgres.render.com',
+//     database: 'ems_6oyk',
+//     password: 'SgAFmlnnVtIp6AeglxUevzertTWBOtTw',
+//     port: 5432,
+// })
+
 
 // testing the connection
-
 pool.connect((err, ) => {
     if (err) {
         return console.error('Error acquiring client', err.stack)
