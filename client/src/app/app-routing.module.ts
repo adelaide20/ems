bcb0c0a01b0af2ac701472d41e4dc830df717b84
adelaide20 <adelaiderorisang@gmail.com>
2023-11-10 13:40:14 +0200

    cliend-side structure

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrivatePagesComponent } from './layout/private-pages/private-pages.component';
+import { DashboardPagesComponent } from './pages/dashboard-pages/dashboard-pages.component';
+import { EmployeePageComponent } from './pages/employee-page/employee-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'private', component: PrivatePagesComponent, children: [
+      { path: 'dash', component: DashboardPagesComponent },
+      { path: 'employee', component: EmployeePageComponent },
+      { path: '', redirectTo: '/dash', pathMatch: 'full' },
+    ]
+  },
+  { path: '**', component: NotfoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { PrivatePagesComponent } from './layout/private-pages/private-pages.component';
+import { DashboardPagesComponent } from './pages/dashboard-pages/dashboard-pages.component';
+import { EmployeePageComponent } from './pages/employee-page/employee-page.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { ListComponent } from './ui/list/list.component';
+import { CreateComponent } from './ui/create/create.component';
+import { SearchComponent } from './ui/search/search.component';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    PrivatePagesComponent,
+    DashboardPagesComponent,
+    EmployeePageComponent,
+    HeaderComponent,
+    ListComponent,
+    CreateComponent,
+    SearchComponent,
+    NotfoundPageComponent
   ],
   imports: [
     BrowserModule,
