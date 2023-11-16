694bb7094b08e4f082c99edba341e2c21e605ca6
adelaide20 <adelaiderorisang@gmail.com>
2023-11-16 14:18:26 +0200

    forms

--- client/src/app/pages/create/create.component.ts
+++ client/src/app/pages/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,6 +7,22 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+
+  employeeForm = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    email: new FormControl(''),
+    gender: new FormControl(''),
+    contactno: new FormControl('')
+  });
+
+  employmentForm = new FormGroup({
+    position: new FormControl(''),
+    emp_status: new FormControl(''),
+    start_date: new FormControl(''),
+    salary: new FormControl('')
+  });
+
 
   constructor() { }
 
