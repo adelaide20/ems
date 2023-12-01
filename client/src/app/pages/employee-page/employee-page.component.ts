6af1a207ad4d454fca35a502f9fec323de04c017
adelaide20 <adelaiderorisang@gmail.com>
2023-12-01 12:53:15 +0200

    working on delete employee

--- client/src/app/pages/employee-page/employee-page.component.ts
+++ client/src/app/pages/employee-page/employee-page.component.ts
@@ -31,7 +31,29 @@ export class EmployeePageComponent implements OnInit {
 
 
   deleteEmployee(){
-    
+   
+  
+
+    // Swal.fire({
+    //   title: "Are you sure?",
+    //   text: "You won't be able to revert this!",
+    //   icon: "warning",
+    //   showCancelButton: true,
+    //   confirmButtonColor: "#3085d6",
+    //   cancelButtonColor: "#d33",
+    //   confirmButtonText: "Yes, delete it!"
+    // }).then((result) => {
+    //   if (result.isConfirmed) {
+    // this.empserv.deleteEmployee(this.emp_id)
+    //     Swal.fire({
+    //       title: "Deleted!",
+    //       text: "Your file has been deleted.",
+    //       icon: "success"
+    //     });
+    //   }
+    // });
+
+
   }
 
 }
